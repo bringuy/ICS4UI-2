a8baa5633dbb2756084ad88d6d3fbcc0d7fd9c7b
Soyeon Jang <43753096+soyeonjangg@users.noreply.github.com>
2019-03-08 09:16:57 -0500

    Update fibonacciSequence.java

--- Recursion Method/fibonacciSequence.java
+++ Recursion Method/fibonacciSequence.java
@@ -1,12 +1,23 @@
+import java.util.*;
+
 public class fibonacciSequence {
 
 	public static void main(String[] args) {
-		int a = 1;
-		int b = 1;
-		System.out.println(fibonacci(a, b));
+		Scanner myInput = new Scanner(System.in);
+		
+		int n = myInput.nextInt();
+		System.out.println(fibonacci(n));
+		myInput.close();
 	}
 
-	public static int fibonacci(int a, int b) {
-		return fibonacci(a, a + b);
+	public static int fibonacci(int n) {
+		if ( n==1){
+			return 1; 
+		}
+		else if (n==2){
+			return 1; 
+		}
+		else 
+			return (fibonacci(n-1)+fibonacci(n-2));		
 	}
 }
